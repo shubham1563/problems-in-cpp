3b4cf68a1c6cb707f62b0ff9f527069bac91d202
shubham1563 <shubhammali1563@.com>
2024-04-24 22:48:25 +0530

    first commit

---
+++ str.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+string sortstring(string str)
+{
+    string newstr = "";
+    for (int i = 0; i < str.length(); i++)
+    {
+        for (int j = i + 1; j < str.length(); j++)
+        {
+            if (str[i] > str[j])
+            {
+                int temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+        newstr += str[i]; // newstr=newstr+str[i]  (concatinate)
+    }
+    return newstr;
+}
+int main()
+{
+    string str = "dbac";
+    cout << sortstring(str);
+    return 0;
+}
+
